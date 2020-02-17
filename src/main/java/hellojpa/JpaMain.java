417b966c2d33aa77a3a2ed3dd325fc4d40abb03a
yoonemong <yoonemong@naver.com>
2020-02-17 23:06:39 +0900

    jpa scheme

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,27 +15,13 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //비영속
-          /*  Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
 
-            //영속
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            System.out.println("=== AFTER ===");*/
+            Member member = em.find(Member.class,100L);
+            member.setName("AAAA");
 
-         /*   Member findMember = em.find(Member.class,101L);
+            em.clear();
 
-            System.out.println("findMember.id =" + findMember.getId());
-            System.out.println("findMember.name =" + findMember.getName());*/
-
-       /* Member member = em.find(Member.class,150L);
-        member.setName("ZZZZZ");*/
-
-       Member member = new Member(200L,"member200");
-       em.persist(member);
-       em.flush();
+            Member member2 = em.find(Member.class,100L);
 
        System.out.println("==================");
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,14 +1,19 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true,length = 10)
     private String name;
+    private int age;
 
     public Member(){
 
@@ -33,5 +38,13 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
