6f6b6a95c9ddd4fc1a49de51bd181447a62477f1
yoonemong <yoonemong@naver.com>
2020-02-26 07:11:44 +0900

    pk 공부

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,35 +6,20 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator",
+sequenceName = "member_seq")
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "member_seq_generator")
     private Long id;
 
     @Column(name="name" , nullable = false)
     private String username; //DB 컬럼명은 name 객체의 이름은 username
 
-    private Integer age;
-
-    //@Enumerated(EnumType.STRING) //enum을 쓰고 싶당 STRING을 써야한다. ORDINAL을 사용하면 안된다.
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType; //DB에는
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate createDate2;
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
 
     public Member(){
     }
-
 
     public Long getId() {
         return id;
@@ -50,45 +35,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
